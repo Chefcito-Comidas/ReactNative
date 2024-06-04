e65b3ba6e8bfa7fd9336ad3e35068a8ae362ae32
Santiago39353535 <santiagomarinaro1@gmail.com>
2024-06-04 10:37:10 -0300

    request con axios

---
+++ chefcito/api/apiRestWrapper.ts
@@ -0,0 +1,94 @@
+import axios, { AxiosError, AxiosResponse, InternalAxiosRequestConfig } from "axios";
+import { BASE_URL, REQUEST_TIMEOUT } from "../utils/constants";
+
+interface Options {
+    url: string;
+    customHeaders?: any;
+    params?: any;
+    payload?: Object;
+}
+
+const getHeaders = (customHeaders: any) => {
+    return {
+        ...customHeaders,
+        Accept: "application/json",
+        "Content-Type": "application/json",
+        token: "test-x"
+    }
+};
+
+axios.defaults.baseURL = BASE_URL;
+axios.defaults.timeout = REQUEST_TIMEOUT;
+
+const requestInterceptor = (req: InternalAxiosRequestConfig<any>): InternalAxiosRequestConfig<any> | Promise<InternalAxiosRequestConfig<any>> => {
+    console.log("request wroks");
+    return req;
+};
+
+const successInterceptor = (res: AxiosResponse): AxiosResponse => {
+    console.log("success wroks");
+    return res;
+};
+
+const errorInterceptor = (err: AxiosError) => {
+    return Promise.reject(err);
+};
+
+axios.interceptors.request.use(requestInterceptor);
+axios.interceptors.response.use((res) => successInterceptor(res), (err) => errorInterceptor(err));
+
+export const apiGet = <T>({ url, customHeaders, params }: Options): Promise<T> => {
+    return new Promise((resolve, reject) => {
+        axios
+            .get<T>(url, { headers: getHeaders(customHeaders), params: params })
+            .then((response: AxiosResponse) => {
+                console.log("resolving data");
+                resolve(response.data);
+            })
+            .catch((error: AxiosError) => {
+                reject(error);
+            });
+    });
+};
+
+export const apiPost = <T>({ url, payload, customHeaders, params }: Options): Promise<T> => {
+    return new Promise((resolve, reject) => {
+        axios
+            .post<T>(url, payload, { headers: getHeaders(customHeaders), params: params })
+            .then((response: AxiosResponse) => {
+                console.log("resolving data");
+                resolve(response.data);
+            })
+            .catch((error: AxiosError) => {
+                reject(error);
+            });
+    });
+};
+
+export const apiPut = <T>({ url, payload, customHeaders, params }: Options): Promise<T> => {
+    return new Promise((resolve, reject) => {
+        axios
+            .put<T>(url, payload, { headers: getHeaders(customHeaders), params: params })
+            .then((response: AxiosResponse) => {
+                console.log("resolving data");
+                resolve(response.data);
+            })
+            .catch((error: AxiosError) => {
+                reject(error);
+            });
+    });
+};
+
+export const apiDelete = <T>({ url, customHeaders, params }: Options): Promise<T> => {
+    return new Promise((resolve, reject) => {
+        axios
+            .delete<T>(url, { headers: getHeaders(customHeaders), params: params })
+            .then((response: AxiosResponse) => {
+                console.log("resolving data");
+                resolve(response.data);
+            })
+            .catch((error: AxiosError) => {
+                reject(error);
+            });
+    });
+};
