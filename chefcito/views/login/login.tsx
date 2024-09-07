e6c8d8d1f6074b1a9164f873004626960314eef2
Santiago39353535 <santiagomarinaro1@gmail.com>
2024-09-07 19:06:22 -0300

    google login

--- chefcito/views/login/login.tsx
+++ chefcito/views/login/login.tsx
@@ -1,34 +1,63 @@
 // Formik x React Native example
-import React from 'react';
-import { Button, TextInput, View,StyleSheet, Text, Pressable, Image } from 'react-native';
-import { Formik } from 'formik';
-import * as yup from 'yup';
-import { loginUserPassword } from '../../api/googleAuth';
-import { COLORS } from '../../utils/constants';
+import React, { useState } from "react";
+import {
+  Button,
+  TextInput,
+  View,
+  StyleSheet,
+  Text,
+  Pressable,
+  Image,
+} from "react-native";
+import { Formik } from "formik";
+import * as yup from "yup";
+import {
+  loginUserPassword,
+  loginWithGoogleCredentials,
+} from "../../api/googleAuth";
+import { COLORS } from "../../utils/constants";
+import {
+  GoogleSignin,
+  GoogleSigninButton,
+} from "@react-native-google-signin/google-signin";
+import auth from "@react-native-firebase/auth";
 
 export const LogInForm = ({ navigation }) => {
-    const onLogin = async (values) => {
-        const user = await loginUserPassword(values.email, values.password);
-        if (user) {
-            console.log('log in exitoso');
-        } else {
-            console.log('log in error');
-        }
+  const onLogin = async (values) => {
+    const user = await loginUserPassword(values.email, values.password);
+    if (user) {
+      console.log("log in exitoso");
+    } else {
+      console.log("log in error");
     }
+  };
 
-    const schema = yup.object().shape({
-        email: yup.string().required(),
-        password: yup.string().required(),
-    });
+  const schema = yup.object().shape({
+    email: yup.string().required(),
+    password: yup.string().required(),
+  });
 
-    const goToSignIn = () => {
-        navigation.navigate('SignIn');
+  const signin = async () => {
+    try {
+      await GoogleSignin.hasPlayServices();
+      const user = await GoogleSignin.signIn();
+      await loginWithGoogleCredentials(user.data.idToken);
+    } catch (e) {
+      console.log("error", e);
     }
+  };
 
-    return (
-        <View style={styles.container}>
-        <Image source={require('../../assets/images/logochefcito.jpg')} style={styles.logo} />
-            <Formik
+  const goToSignIn = () => {
+    navigation.navigate("SignIn");
+  };
+
+  return (
+    <View style={styles.container}>
+      <Image
+        source={require("../../assets/images/logochefcito.jpg")}
+        style={styles.logo}
+      />
+      {/* <Formik
                 validationSchema={schema}
                 initialValues={{ email: '', password: '' }}
                 onSubmit={onLogin}
@@ -57,60 +86,65 @@ export const LogInForm = ({ navigation }) => {
                         </Pressable>
                     </View>
                 )}
-            </Formik>
-            <Pressable onPress={goToSignIn}>
-                <Text style={styles.signInText}>¿No tienes cuenta? Regístrate</Text>
-            </Pressable>
-        </View>
-    );
-}
+            </Formik> */}
+      <GoogleSigninButton
+        size={GoogleSigninButton.Size.Standard}
+        color={GoogleSigninButton.Color.Dark}
+        onPress={signin}
+      />
+      <Pressable onPress={goToSignIn}>
+        <Text style={styles.signInText}>¿No tienes cuenta? Regístrate</Text>
+      </Pressable>
+    </View>
+  );
+};
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: COLORS.pastellog,
-        alignItems: 'center',
-        justifyContent: 'center',
-        padding: 20,
-    },
-    logo: {
-        width: 200,
-        height: 200,
-        marginBottom: 30,
-    },
-    formContainer: {
-        width: '100%',
-        alignItems: 'center',
-    },
-    formField: {
-        borderWidth: 1,
-        borderColor: COLORS.pastelborder,
-        backgroundColor: '#fff',
-        borderRadius: 10,
-        marginBottom: 15,
-        paddingHorizontal: 15,
-        paddingVertical: 10,
-        minWidth: 250,
-        color: '#333',
-        fontSize: 16,
-    },
-    button: {
-        backgroundColor: '#ffa726',
-        borderRadius: 10,
-        paddingVertical: 10,
-        paddingHorizontal: 20,
-        alignItems: 'center',
-        justifyContent: 'center',
-        marginTop: 10,
-    },
-    buttonText: {
-        color: '#fff',
-        fontWeight: 'bold',
-        fontSize: 18,
-    },
-    signInText: {
-        marginTop: 20,
-        color: '#333',
-        fontSize: 14,
-    },
+  container: {
+    flex: 1,
+    backgroundColor: COLORS.pastellog,
+    alignItems: "center",
+    justifyContent: "center",
+    padding: 20,
+  },
+  logo: {
+    width: 200,
+    height: 200,
+    marginBottom: 30,
+  },
+  formContainer: {
+    width: "100%",
+    alignItems: "center",
+  },
+  formField: {
+    borderWidth: 1,
+    borderColor: COLORS.pastelborder,
+    backgroundColor: "#fff",
+    borderRadius: 10,
+    marginBottom: 15,
+    paddingHorizontal: 15,
+    paddingVertical: 10,
+    minWidth: 250,
+    color: "#333",
+    fontSize: 16,
+  },
+  button: {
+    backgroundColor: "#ffa726",
+    borderRadius: 10,
+    paddingVertical: 10,
+    paddingHorizontal: 20,
+    alignItems: "center",
+    justifyContent: "center",
+    marginTop: 10,
+  },
+  buttonText: {
+    color: "#fff",
+    fontWeight: "bold",
+    fontSize: 18,
+  },
+  signInText: {
+    marginTop: 20,
+    color: "#333",
+    fontSize: 14,
+  },
 });
